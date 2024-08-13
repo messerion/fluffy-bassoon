2062af1ababf9b988583ca00add1ebc9cb1d6234
messerion <irka1395@gmail.com>
2024-08-13 14:28:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-a='Топинамбур'
-print(a[0])
-print(a[-1])
-print(a[5:])
-print(a[::-1])
-print(a[1::2])
+a='топинамбур'
+b=len(a)//2
+print (a [ 0 ])
+print (a [ - 1 ])
+print (a [b::])
+print (a [:: - 1 ])
+print (a [ 1 :: 2 ])
