2715d2c8bdd8383cd684802e54004da0e644ff7a
messerion <irka1395@gmail.com>
2024-09-07 14:30:04 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,23 @@
+def send_email(message,recipient,*,sender = "university.help@gmail.com"):
+
+    if  ('@' not in recipient and '@' not in sender or
+            (not recipient.endswith('.com') and not recipient.endswith('.ru') and not recipient.endswith('.net')) or
+            (not sender.endswith('.com') and not sender.endswith('.ru') and not sender.endswith('.net'))):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+        return
+
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+        return
+
+
+    elif sender == 'university.help@gmail.com':
+
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+        return
+    print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
